86b8ca9b85b8200405645ca7cc318673ddb78c86
rib-bruno <ribeirobrunosouza@hotmail.com>
2023-03-01 16:03:49 -0300

    inicio da implementação da UI

---
+++ app/src/main/java/com/example/previsodotempoapp/presentation/WeatherCard.kt
@@ -0,0 +1,64 @@
+package com.example.previsodotempoapp.presentation
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.time.format.DateTimeFormatter
+
+@Composable
+fun WeatherCard(
+    state: WeatherState,
+    backGroundColor: Color,
+    modifier: Modifier = Modifier
+
+) {
+
+    //só mostrar os dados no card se o estado meteorológico não for nulo
+    state.weatherInfo?.currentWeatherData?.let { data ->
+        Card(
+            backgroundColor = backGroundColor,
+            shape = RoundedCornerShape(10.dp),
+            modifier = modifier.padding(16.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Today ${
+                        data.time.format(
+                            DateTimeFormatter.ofPattern("HH:mm")
+                        )
+                    }",
+                    //alinhando
+                    modifier = Modifier.align(Alignment.End),
+                    color = Color.White
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Image(
+                    painter = painterResource(id = data.weatherType.iconRes),
+                    contentDescription = null,
+                    modifier = Modifier.width(200.dp)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = "${data.temperatureCelsius}°C",
+                    fontSize = 50.sp,
+                    color = Color.White
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+        }
+    }
+}
